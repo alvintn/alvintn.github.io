dff94be2fea6624a72036abe0a9de9df8c712c6f
Alvin Nguyen <alvintn@uci.edu>
2018-01-17 16:06:52 -0800

    Removed JS and added background.
    replace js with some css tricks and added mobile support with scaling
    font with screen width.

--- hovermove.js
+++ hovermove.js
@@ -14,13 +14,14 @@ $(document).ready(function() {
 
 
   function navigate() {
-    $('#links').css({"left":"0px"});
+    $('#links').css({"left":"0px", "display":"block"});
     $('.title').animate({"margin-left":"-90px"}, 400);
     $('.myname').animate({"margin-top":"-85px"}, 400);
     $('#links').animate({"opacity":"1"}, 1500);
   }
 
   function navigate_main() {
+    $('#links').css({"display":"block"});
     $('.title').animate({"margin-left":"-62px"}, 400);
     $('#links').animate({"opacity":"1"}, 1500);
   }
