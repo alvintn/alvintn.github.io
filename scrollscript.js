6aeee1802b4eb8fc8cd921788c941aec6df2c74b
Alvin Nguyen <alvintn@uci.edu>
2018-01-22 17:12:48 -0800

    Improved scroll js
    changed scroll js to switch between classes and hide none viewable assests when document ready

--- scrollscript.js
+++ scrollscript.js
@@ -1,15 +1,23 @@
 $(document).ready(function() {
+  $('.textbox').each(function(index) {
+
+    console.log("index: " + index);
+    var middle_of_object = $(this).position().top + $(this).outerHeight() / 2;
+    var bottom_of_window = $(window).scrollTop() + $(window).height();
+    /* If the object is not visible in the window, hide */
+    if( bottom_of_window < middle_of_object ){
+        $(this).attr("class", "hidden");
+    }
+  });
   $(window).scroll(function() {
     $('.hidden').each(function(index) {
 
       console.log("index: " + index);
-      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var middle_of_object = $(this).position().top + $(this).outerHeight() / 6;
       var bottom_of_window = $(window).scrollTop() + $(window).height();
-      /* If the object is completely visible in the window, fade it in */
-      if( bottom_of_window > bottom_of_object ){
-          $(this).animate({opacity: 1}, 800);
-          $(this).attr("class", "shown");
-          //$(this).slideToggle();
+      /* If the object is somewhat visible in the window, switch class */
+      if( bottom_of_window > middle_of_object ){
+          $(this).attr("class", "textbox");
       }
     });
   });
